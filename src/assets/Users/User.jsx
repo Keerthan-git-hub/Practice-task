0e2254752a3ea80e6fe700f60038e85e23fcacce
keerthan nethikar <keerthan.nethikar@gmail.com>
2025-06-06 22:22:30 +0530

    practice

--- src/assets/Users/User.jsx
+++ src/assets/Users/User.jsx
@@ -6,10 +6,10 @@ const User = ({singleP}) => {
     <div className='user_card'>
         <img src={singleP.img} alt="" />
         <h4>{singleP.name}</h4>
-        <p>Email: {singleP.email}</p>
-        <p>City: {singleP.address.street}, {singleP.address.city}</p>
-        <p>Company Name: {singleP.company.name}</p>
-        <p>Phone: {singleP.phone}</p>
+        <p><strong>Email:</strong> {singleP.email}</p>
+        <p><strong>City: </strong> {singleP.address.street}, {singleP.address.city}</p>
+        <p><strong>Company Name: </strong>{singleP.company.name}</p>
+        <p><strong>Phone: </strong> {singleP.phone}</p>
     </div>
   )
 }
